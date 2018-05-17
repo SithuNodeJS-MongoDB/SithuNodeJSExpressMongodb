1488690083182aeb1b20ed67eda44234937a98fd
Sitharthan <6356904+sitharthan@users.noreply.github.com>
2018-05-18 00:23:32 +0530

    GET Userlist page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/SithuNodeJSExpressMongodb');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
