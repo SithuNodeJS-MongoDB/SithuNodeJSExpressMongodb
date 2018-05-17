7850e965b3f94c9326c28950886f50d3dafdd03e
Sitharthan <6356904+sitharthan@users.noreply.github.com>
2018-05-18 00:18:44 +0530

    Make our db accessible to our router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var mongo = require('mongodb');
 var monk = require('monk');
+var db = monk('localhost:27017/SithuNodeJSExpressMongodb');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -20,6 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
