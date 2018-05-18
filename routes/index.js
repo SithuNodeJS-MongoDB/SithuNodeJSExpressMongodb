f98e301b55a5bb235169d9aa11f118db6d760226
Sitharthan <6356904+sitharthan@users.noreply.github.com>
2018-05-18 19:18:59 +0530

    Changes: Implemented logics for adding new user

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,57 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* GET Hello World page. */
-router.get('/helloworld', function(req,res){
-  res.render('helloworld', {title: 'Hello, World!'});
+router.get('/helloworld', function (req, res) {
+  res.render('helloworld', { title: 'Hello, World!' });
 });
 
 /* GET Userlist page. */
-router.get('/userlist', function(req,res){
+router.get('/userlist', function (req, res) {
   var db = req.db;
   var collection = db.get('usercollection');
-  collection.find({},{},function(e,docs){
-    res.render('userlist',{
-      "userlist" : docs
+  collection.find({}, {}, function (e, docs) {
+    res.render('userlist', {
+      "userlist": docs
     });
   });
 });
 
 /* GET New USer page. */
-router.get('/newuser', function(req,res){
-  res.render('newuser',{title:'Add New User'});
+router.get('/newuser', function (req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function (req, res) {
+  // Set our internal DB variable
+  var db = req.db;
+
+  // Get our form values, Thease rely on the "name" attributes
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+  console.log("userName is  : ", userName);
+  console.log("userEmail is : ", userEmail);
+  //Set our collection
+  var collection = db.get('usercollection');
+
+  // Submit to the DB
+  collection.insert({
+    "username": userName,
+    "email": userEmail
+  }, function (err, doc) {
+    if (err) {
+      // If it failed, return error
+      res.send("There was a problem adding the information to the database.");
+    } else {
+      // And forward to success page
+      res.redirect("userlist");
+    }
+  });
 });
 
 module.exports = router;
