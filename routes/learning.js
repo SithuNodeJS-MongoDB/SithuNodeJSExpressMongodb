3efca520ff012bcc1a265c65f93f16d2b925b31c
Sitharthan <6356904+sitharthan@users.noreply.github.com>
2018-05-18 21:44:08 +0530

    Changes for Adding url with details to db and display the error message if user entered same url as before added

--- routes/learning.js
+++ routes/learning.js
@@ -5,4 +5,49 @@ router.get('/',function(req,res,next){
     res.render('addDetails');
 });
 
+/* To submit the current form details to DB */
+router.post('/addFormDetails',function(req,res,next){
+    console.log("i am in /addFormDetails");
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var newUrl = req.body.learningurl;
+    var newTitle = req.body.learningtitle;
+
+    // Set our collection
+    var collection = db.get('detailscollection');
+
+    //Submit to DB
+    //Checking duplicate entries
+    collection.findOne({learningurl:newUrl},function(err,result){
+        if(err) throw err;
+        if(result === null){
+            //Submit the data now
+            collection.insert({
+                "learningurl" : newUrl,
+                "learningtitle" : newTitle
+            }, function(err, docs){
+                if(err){
+                    //If it is failed, return error
+                    res.send("There was a problem adding the information to the database");
+                }else{
+                    console.log("Current doc is : ", docs);
+                    //And forward to success page with all data
+                    collection.find({}, {}, function (e, docs) {
+                        res.render('details', {
+                          "detailslist": docs
+                        });
+                      });
+                }
+            });
+        }else if(result.learningurl === newUrl){
+            res.render("duplicate",{"statusMsg":"Current Entering data available in database"});
+            db.close();
+        }else{
+            res.send("Something went to wrong");
+            db.close();
+        }
+    });
+});
 module.exports = router;
